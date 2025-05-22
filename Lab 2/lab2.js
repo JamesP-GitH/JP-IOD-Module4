6e663c23a51ff5a467221af154455917eed45faa
JamesP-GitH <james.pagliaccio@gmail.com>
2025-05-23 09:49:35 +1000

    added keeping button active on click

--- Lab 2/lab2.js
+++ Lab 2/lab2.js
@@ -1,6 +1,10 @@
 let currentOperator = null;
 
-function setOperator(operator) {
+function setOperator(operator, buttonElement) {
     currentOperator = operator;
     console.log("Operator set to:", currentOperator);
+
+    const allOperators = document.querySelectorAll(".opBtn");
+    allOperators.forEach((btn) => btn.classList.remove("active"));
+    buttonElement.classList.add("active");
 }
